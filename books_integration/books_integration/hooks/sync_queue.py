b31b3809645a21cd74466626ec9d110223a6b7ca
akshayitzme <akshaypooloth1@gmail.com>
2024-11-13 10:41:42 +0000

    fix: doctype names

--- books_integration/books_integration/hooks/sync_queue.py
+++ books_integration/books_integration/hooks/sync_queue.py
@@ -24,22 +24,19 @@ def add_doc_to_sync_queue(doc, method):
 
 def document_should_sync(doc):
     doctype = doc.doctype
-
-    if not doc.get("should_add_to_sync_queue"):
-        return False
     
-    is_sync_enabled = frappe.db.get_single_value("FBooks Sync Settings", "enable_sync")
+    is_sync_enabled = frappe.db.get_single_value("Books Sync Settings", "enable_sync")
 
     if doctype == "Item Price":
         is_sync_enabled_for_price_list = frappe.db.get_single_value(
-            "FBooks Sync Settings", "sync_price_list"
+            "Books Sync Settings", "sync_price_list"
         )
 
         return is_sync_enabled and is_sync_enabled_for_price_list
 
     formatted_doctype_name = "_".join(doctype.lower().split(" "))
     is_sync_enabled_for_doctype = frappe.db.get_single_value(
-        "FBooks Sync Settings", "sync_{}".format(formatted_doctype_name)
+        "Books Sync Settings", "sync_{}".format(formatted_doctype_name)
     )
 
     return is_sync_enabled and is_sync_enabled_for_doctype
