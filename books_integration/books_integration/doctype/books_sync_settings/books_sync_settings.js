a93a6351d2013b80a1d8cfd9f58c8b63a5b4c496
akshayitzme <akshaypooloth1@gmail.com>
2024-11-13 07:16:44 +0000

    feat: sync settings doctype

---
+++ books_integration/books_integration/doctype/books_sync_settings/books_sync_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Akshay and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Books Sync Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
