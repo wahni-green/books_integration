26dc30e3e1ae8c69d22971d4ecd391ef383a40cd
Dany Robert <danyrt@wahni.com>
2024-12-19 05:01:48 +0000

    fix: retry on error

--- books_integration/books_integration/doctype/books_error_log/books_error_log.js
+++ books_integration/books_integration/doctype/books_error_log/books_error_log.js
@@ -1,8 +1,10 @@
 // Copyright (c) 2024, Wahni IT Solutions and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Books Error Log", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Books Error Log", {
+	refresh(frm) {
+        frm.add_custom_button(__("Retry"), function () {
+            frm.call("retry_processing");
+        });
+	},
+});
--- books_integration/books_integration/doctype/books_error_log/books_error_log.py
+++ books_integration/books_integration/doctype/books_error_log/books_error_log.py
@@ -1,9 +1,18 @@
 # Copyright (c) 2024, Wahni IT Solutions and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+import json
 from frappe.model.document import Document
+from books_integration.scheduler import process_data
 
 
 class BooksErrorLog(Document):
-	pass
+	@frappe.whitelist()
+	def retry_processing(self):
+		data = json.loads(self.data)
+		process_data(
+			self.books_instance, data, self.document_type
+		)
+		frappe.msgprint("Processed")
+		self.delete()
