05a807c8f8e1983780d1fcccbf9751c9c4eb705a
stevewilson9009 <stevew9009@gmail.com>
2025-05-09 13:24:22 +0200

    chore(patch): add checkbox to POS Opening and Closing Entries

---
+++ books_integration/patches/pos_fields.py
@@ -0,0 +1,43 @@
+# Copyright (c) 2025, Wahni IT Solutions and contributors
+# For license information, please see license.txt
+
+from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
+
+
+def execute():
+    create_custom_fields(
+        {
+            "POS Opening Entry": [
+                {
+                    "fieldname": "from_frappebooks",
+                    "label": "From FrappeBooks",
+                    "fieldtype": "Check",
+                    "insert_after": "period_end_date",
+                },
+                {
+                    "fieldname": "books_instance",
+                    "label": "Books Instance",
+                    "fieldtype": "Link",
+                    "options": "Books Instance",
+                    "insert_after": "from_frappebooks",
+                    "read_only": 1,
+                },
+            ],
+            "POS Closing Entry": [
+                {
+                    "fieldname": "from_frappebooks",
+                    "label": "From FrappeBooks",
+                    "fieldtype": "Check",
+                    "insert_after": "period_end_date",
+                },
+                {
+                    "fieldname": "books_instance",
+                    "label": "Books Instance",
+                    "fieldtype": "Link",
+                    "options": "Books Instance",
+                    "insert_after": "from_frappebooks",
+                    "read_only": 1,
+                },
+            ],
+        }
+    )
