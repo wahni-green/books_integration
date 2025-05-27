517ee4a415052c4d79e1b0010ccb40ad053a8cc6
stevewilson9009 <stevew9009@gmail.com>
2025-05-27 14:00:40 +0200

    disable pricing rule

--- books_integration/hooks.py
+++ books_integration/hooks.py
@@ -146,7 +146,7 @@ doc_events = {
     },
     "Item Price": {"on_update": "books_integration.sync_queue.add_item"},
     "Batch": {"on_update": "books_integration.sync_queue.add_doc_to_sync_queue"},
-    "Pricing Rule": {"on_update": "books_integration.sync_queue.add_doc_to_sync_queue"},
+    # "Pricing Rule": {"on_update": "books_integration.sync_queue.add_doc_to_sync_queue"},
 }
 
 # Scheduled Tasks
